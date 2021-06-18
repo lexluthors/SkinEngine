cdfae3b6466467f0e4c6ad80a72d57fbc9350207
liujie <594134383@qq.com>
2021-06-18 14:58:46 +0800

    优化demo

--- app/src/main/java/com/gyzq/skinengine/MainActivity.java
+++ app/src/main/java/com/gyzq/skinengine/MainActivity.java
@@ -6,6 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.gyzq.skin.SkinManager;
+import com.hjq.permissions.OnPermissionCallback;
+import com.hjq.permissions.Permission;
+import com.hjq.permissions.XXPermissions;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        String[] permissionStrs = {Permission.WRITE_EXTERNAL_STORAGE,Permission.READ_EXTERNAL_STORAGE};
+        XXPermissions.with(this)
+                .permission(permissionStrs)
+                .request(new OnPermissionCallback() {
+                    @Override
+                    public void onGranted(List<String> permissions, boolean all) {
+
+                    }
+                });
+
+
+
+
+
         //app-debug.apk
         findViewById(R.id.huanfu).setOnClickListener(new View.OnClickListener() {
             @Override
